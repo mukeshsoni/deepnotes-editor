944e6040e5c9d6bb7256db5ad073d51e93df11c8
Mukesh Soni <mukeshsoni@gmail.com>
2020-08-15 19:24:17 +0530

    extract all css into a file so that users can just import that css file like we import the draftjs css

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,7 +2,7 @@ const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
   rollup(config) {
-    config.plugins.push(postcss());
+    config.plugins.push(postcss({ extract: 'deepnotes-editor.css' }));
     return config;
   },
 };
