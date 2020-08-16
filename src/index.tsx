893ce4816059ca925902e19caf5a18527a9a3f57
Mukesh Soni <mukeshsoni@gmail.com>
2020-08-16 20:04:45 +0530

    expose createDecorators function

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import Editor from './Editor/components/Editor';
 export { ROOT_KEY } from './constants';
 export { findParent } from './Editor/find_parent';
 export { default as pluckGoodies } from './Editor/pluck_goodies';
+export { createDecorators } from './Editor/decorators';
 
 export default Editor;
