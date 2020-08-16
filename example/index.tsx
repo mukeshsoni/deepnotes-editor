687c9e121a2ef7bf88c32f27e7b9205a1f7e0c05
Mukesh Soni <mukeshsoni@gmail.com>
2020-08-16 19:54:35 +0530

    remove unused code from example/index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -3,8 +3,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Editor from '../.';
 
-// import '../src/tailwind_generated.css';
-// import '../src/Editor/components/editor_styles.css';
 // If i import the deepnotes-editor.css file directly from ../dist/, it
 // doesn't apply any of the tailwind css. Don't know why.
 import './deepnotes-editor.css';
