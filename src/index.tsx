983053c7b74c64e56f609063fb9173eaf5ab82b4
Mukesh Soni <mukeshsoni@gmail.com>
2020-08-16 19:54:10 +0530

    expose some useful stuff from the editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
 import Editor from './Editor/components/Editor';
+export { ROOT_KEY } from './constants';
+export { findParent } from './Editor/find_parent';
+export { default as pluckGoodies } from './Editor/pluck_goodies';
 
 export default Editor;
