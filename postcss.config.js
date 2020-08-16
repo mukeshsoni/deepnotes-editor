5a3dbe1d795fd7d24d9113d58e077a9cb426ed47
Mukesh Soni <mukeshsoni@gmail.com>
2020-08-16 09:54:50 +0530

    looks like tsdx already has support for css modules even without postcss-module. using postcss-modules was applying css classname hashing twice.

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  modules: true,
+  // it looks like tsdx already has support for css-modules using some other
+  // rollup plugin. If i use postcss-modules plugin again, the classnames
+  // for module css are generated twice. Someone is creating a hashed version
+  // of a class and then postcss-modules plugin creates another classname
+  // from that hashed classname
+  modules: false,
   plugins: {
-    'postcss-modules': {
-      globalModulePaths: [
-        // Put your global css file paths.
-        'src/tailwind_generated.css',
-        'src/Editor/components/editor_styles.css',
-      ],
-    },
+    // 'postcss-modules': {
+    // globalModulePaths: [
+    // // Put your global css file paths.
+    // /.*global\.css$/,
+    // 'src/tailwind_generated.css',
+    // ],
+    // },
   },
 };
